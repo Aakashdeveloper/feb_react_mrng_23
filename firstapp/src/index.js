0508d7d210ced8af5963e15c25dd1235f1286584
aakashdeveloper <ahanda205@gmail.com>
2023-02-06 20:44:49 +0530

    first component

---
+++ firstapp/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return(
+        <h1>Hii From React</h1>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
+
+
+// var React = require('react')
+// var ReactDOM = require('react-dom')
+
+// function App(){
+//     return(
+//         <h1>Hii From React</h1>
+//     )
+// }
---
+++ learnPackage/Readme.md
@@ -0,0 +1,1 @@
+# Details description of the app
