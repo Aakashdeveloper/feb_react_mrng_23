90677b9f7bc4efc2f922994116dccac38f12500e
aakashdeveloper <ahanda205@gmail.com>
2023-03-06 21:22:31 +0530

    routing5

---
+++ routing5/src/component/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="panel panel-primary">
+            <div className="panel-heading">
+                Home Page
+            </div>
+            <div className="panel-body">
+                <h1>Home Page</h1>
+                <p>
+                    Home Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ routing5/src/component/Post.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Post = () => {
+    return(
+        <div className="panel panel-info">
+            <div className="panel-heading">
+            Post Page
+            </div>
+            <div className="panel-body">
+                <h1>Post Page</h1>
+                <p>
+                    Post Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
