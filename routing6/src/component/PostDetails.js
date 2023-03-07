2cc5a1c5b104f717f60273fe0416e97eefa3cbe9
aakashdeveloper <ahanda205@gmail.com>
2023-03-07 20:46:06 +0530

    routing6

--- routing6/src/component/PostDetails.js
+++ routing6/src/component/PostDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const PostDetails = (props) => {
-    console.log(props.location.search)
-    console.log(props.location.search.split('=')[1])
+    // console.log(props.location.search)
+    // console.log(props.location.search.split('=')[1])
     //console.log(props.match.params.topic)
     return(
         <div className="panel panel-warning">
-            <div className="panel-heading">
+            <h1>Post Details</h1>
+            {/* <div className="panel-heading">
                 {props.match.params.topic} Details Page
             </div>
             <div className="panel-body">
@@ -15,7 +16,7 @@ const PostDetails = (props) => {
                     {props.match.params.topic} Details Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 </p>
                 <h2>You are on Page Number {props.location.search.split('=')[1]}</h2>
-            </div>
+            </div> */}
         </div>
     )
 }
---
+++ routing6/src/component/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="panel panel-danger">
+            <div className="panel-heading">
+            Profile Page
+            </div>
+            <div className="panel-body">
+                <h1>Profile Page</h1>
+                <p>
+                Profile Page is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
