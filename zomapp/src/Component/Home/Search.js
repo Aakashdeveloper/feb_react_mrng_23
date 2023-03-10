af7472ad96e98eea3495b48045328ef9206bec63
aakashdeveloper <ahanda205@gmail.com>
2023-03-10 21:02:00 +0530

    searchapi

--- zomapp/src/Component/Home/Search.js
+++ zomapp/src/Component/Home/Search.js
@@ -1,8 +1,55 @@
 import React,{Component} from 'react';
 import './search.css';
 
+const base_url = "http://3.17.216.66:4000";
 class Search extends Component {
+
+    constructor(){
+        super()
+        console.log("Inside Constructor")
+
+        this.state={
+            location:'',
+            restData:''
+        }
+    }
+
+    renderCity = (data) => {
+        console.log(">>>>renderCity",data)
+        if(data){
+            return data.map((item) => {
+                return(
+                    <option value={item.state_id} key={item.state_id}>
+                        {item.state}
+                    </option>
+                )
+            })
+        }
+    }
+
+    renderRest = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <option value={item.restaurant_id} key={item.restaurant_id}>
+                        {item.restaurant_name} | {item.address}
+                    </option>
+                )
+            })
+        }
+    }
+
+    handleCity = (event) => {
+        let stateId = event.target.value;
+        fetch(`${base_url}/restaurant?stateId=${stateId}`,{method:'GET'})
+        .then((res) =>  res.json())
+        .then((data) => {
+            this.setState({restData:data})
+        })
+    }
+
     render(){
+        console.log("Inside Render")
         return(
             <>
                 <div id="search">
@@ -13,20 +60,39 @@ class Search extends Component {
                         Search Place Near To You
                     </div>
                     <div id="dropdown">
-                        <select>
+                        <select onChange={this.handleCity}>
                             <option>
-                                ---Select City---
+                                ---Select Your City---
                             </option>
+                            {this.renderCity(this.state.location)}
                         </select>
                         <select className="restSelect">
                             <option>
                                 ---Select Restaurants---
                             </option>
+                            {this.renderRest(this.state.restData)}
                         </select>
                     </div>
                 </div>
             </>
         )
+    }
+
+    // api calling on page load
+    componentDidMount(){
+        console.log("Inside componentDidMount")
+        fetch(`${base_url}/location`,{method: 'GET'})
+        // return the promise
+        .then((res) =>  res.json())
+        // return data
+        .then((data) => {
+            this.setState({location:data})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+       
     }
 }
 
