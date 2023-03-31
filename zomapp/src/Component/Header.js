146da6720afd6671bde05ab1d7818290981a398d
aakashdeveloper <ahanda205@gmail.com>
2023-03-31 21:04:14 +0530

    code

--- zomapp/src/Component/Header.js
+++ zomapp/src/Component/Header.js
@@ -8,7 +8,8 @@ class Header extends Component {
         super(props)
 
         this.state={
-            userData:''
+            userData:'',
+            socailData:''
         }
     }
 
@@ -16,30 +17,49 @@ class Header extends Component {
         sessionStorage.removeItem('ltk')
         sessionStorage.removeItem('userInfo');
         sessionStorage.setItem('loginStatus','loggedOut');
+        sessionStorage.setItem('socialLogin','false')
         this.setState({userData:'' })
+        this.setState({socailData:'' })
         this.props.history.push('/')
     }
 
     conditionalHeader = () => {
-        if(this.state.userData.name){
-            let data = this.state.userData;
-            sessionStorage.setItem('userInfo',JSON.stringify(data));
-            sessionStorage.setItem('loginStatus','loggedIn');
-            return(
-                <>
-                    <Link to="/" className="btn btn-success">
-                        <span className="glyphicon glyphicon-user"></span> Hi {data.name}
-                    </Link> &nbsp;
-                    <button onClick={this.handleLogout} className="btn btn-danger">
-                        <span className="glyphicon glyphicon-log-out"></span> Logout
-                    </button>
+        if(this.state.userData.name || this.state.socailData.id){
+            if(sessionStorage.getItem('socialLogin') === "true"){
+                let data = this.state.socailData;
+                let img = data.photos[0].value
+                return(
+                    <>
+                        <Link to="/" className="btn btn-success">
+                            <img src={img} style={{height:30,width:30}}/>&nbsp;
+                            Hi {data.name.givenName}
+                        </Link> &nbsp;
+                        <button onClick={this.handleLogout} className="btn btn-danger">
+                            <span className="glyphicon glyphicon-log-out"></span> Logout
+                        </button>
                 </>
-            )
+                )
+            }else{
+                let data = this.state.userData;
+                sessionStorage.setItem('userInfo',JSON.stringify(data));
+                sessionStorage.setItem('loginStatus','loggedIn');
+                return(
+                    <>
+                        <Link to="/" className="btn btn-success">
+                            <span className="glyphicon glyphicon-user"></span> Hi {data.name}
+                        </Link> &nbsp;
+                        <button onClick={this.handleLogout} className="btn btn-danger">
+                            <span className="glyphicon glyphicon-log-out"></span> Logout
+                        </button>
+                    </>
+                )
+            }
+            
         }else{
             return(
                 <>
-                    <a href="http://localhost:9800/auth/google" className="btn btn-info">
-                        <span className="glyphicon glyphicon-log-in"></span> Login With Google
+                    <a href="http://localhost:9800/auth/google" className="btn">
+                        <img src="https://i.ibb.co/37W2XwW/google.png" style={{height:30,width:30}}/>
                     </a> &nbsp;
                     <Link to="/login" className="btn btn-success">
                         <span className="glyphicon glyphicon-log-in"></span> LogIn
@@ -70,18 +90,39 @@ class Header extends Component {
 
 
     componentDidMount(){
-        fetch(url,{
-            method:'GET',
-            headers:{
-                'x-access-token':sessionStorage.getItem('ltk')
+        if(this.props.location.search){
+            console.log("inside socail login")
+            if(this.props.location.search.split('=')[0]==='?site'){
+                let siteName = this.props.location.search.split('&')[0].split('=')[1]
+                console.log("inside siteName"+siteName)
+                if(siteName === 'google'){
+                    console.log("inside google")
+                    sessionStorage.setItem('socialLogin','true')
+                    let id = this.props.location.search.split('&')[1].split('=')[1];
+                    fetch(`http://localhost:1234/users/${id}`,{method:'GET'})
+                    .then((res) => res.json())
+                    .then((data) => {
+                        this.setState({
+                            socailData:data
+                        })
+                    })
+                }
             }
-        })
-        .then((res) =>  res.json())
-        .then((data) => {
-            this.setState({
-                userData:data
+        }else{
+            fetch(url,{
+                method:'GET',
+                headers:{
+                    'x-access-token':sessionStorage.getItem('ltk')
+                }
             })
-        })
+            .then((res) =>  res.json())
+            .then((data) => {
+                this.setState({
+                    userData:data
+                })
+            })
+        }
+        
     }
     
 }
