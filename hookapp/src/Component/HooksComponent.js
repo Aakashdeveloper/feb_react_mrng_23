c72f4ff691b7a544af6facadf2c796b2d7c0039a
aakashdeveloper <ahanda205@gmail.com>
2023-04-14 20:44:46 +0530

    final

--- hookapp/src/Component/HooksComponent.js
+++ hookapp/src/Component/HooksComponent.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import DisplayComponent from './displayComponent';
+import UseTitle from './customeHooks';
 
 const url = "http://3.17.216.66:4000/restaurants"
 
@@ -9,10 +10,18 @@ function HooksComponent(){
     const [count,setCount] = useState(0)
     const [count1,setCount1] = useState(0);
     const [restaurants, setRestaurants] = useState();
-    
-    const updateCount = () => {
+    const [record,setRecord] = useState(0)
+
+    const updateCount=() =>{
         setCount(count+1)
     }
+    
+    const updateRecord=() =>{
+        setRecord(count+1)
+    }
+    
+
+    UseTitle(record)
 
     useEffect(() => {
         console.log("inside use effect11");
@@ -30,6 +39,9 @@ function HooksComponent(){
             <h1>{title}</h1>
             <h2>{count}</h2>
             <button onClick={updateCount}>
+                Counter
+            </button>
+            <button onClick={updateRecord}>
                 Counter
             </button>
             <h2>{count1}</h2>
